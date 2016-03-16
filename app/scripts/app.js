150cddb679318d0024fae55eea97f7aceb83d8f7
Bluestella <hello@Josephbuluran.com>
2016-03-16 19:48:54 +0800

    initial.

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cautiousWinnerApp
+ * @description
+ * # cautiousWinnerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cautiousWinnerApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngMaterial'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cautiousWinnerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cautiousWinnerApp
+ */
+angular.module('cautiousWinnerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
